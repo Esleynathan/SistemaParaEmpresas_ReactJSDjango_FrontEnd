24b193fe8cc4db4d19449997b784e7d3567822b2
Esley <esleynathan@hotmail.com>
2024-10-17 17:39:42 -0300

    Outras formas de criar componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-const App = () => {
-  return (
-    <div>
-      <h1> Meu Primeiro Componente</h1>
-    </div>
-  )
+// const App = () => {
+//   return (
+//     <div>
+//       <h1> Meu Primeiro Componente</h1>
+//     </div>
+//   )
+// }
+
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Meu Primeiro Projeto</h1>
+//     </div>
+//   )
+// }
+
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Meu primeiro Componente </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
