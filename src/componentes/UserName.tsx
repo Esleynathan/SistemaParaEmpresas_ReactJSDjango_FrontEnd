2c869d8fd4b75f062f1134316afe732c4b9756c3
Esley <esleynathan@hotmail.com>
2024-11-21 16:37:44 -0300

    JSX: variáveis, funções e objetos

--- src/componentes/UserName.tsx
+++ src/componentes/UserName.tsx
@@ -1,5 +1,10 @@
 export const UserName = () => {
+    let name = 'Bago Doce'
+    // let names = {name1: 'BagoDoce', name2: 'Ésley Nathan'}
+
+    // const formatName = (value : string) => value.toUpperCase()
+
     return (
-        <h4>Meu nome é Ésley</h4>
+        <h4>Meu nome é: {name.toUpperCase()}.</h4>
     )
 }
