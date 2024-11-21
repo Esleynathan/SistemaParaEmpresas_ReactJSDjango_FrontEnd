8c24e83b448c1962c756b493876f0deaee95b209
Esley <esleynathan@hotmail.com>
2024-11-21 17:01:41 -0300

    JSX: Renderização condicional

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import { UserInfo } from "./componentes/UserInfo";
 
 const App = () => {
+  const isLogged = false;
+
+  // if (isLogged) {
+  //   return (
+  //     <div>
+  //       <h1>Dados do usuario: </h1>
+
+  //       <UserInfo />
+  //     </div>
+  //   )
+  // }
+
+  // return (
+  //   <div>
+  //     <h1>Não esta logado:</h1>
+  //   </div>
+  // )
+
+
+  // if (!isLogged) {
+  //   return;
+  // }
+
   return (
     <div>
-      <h1>Dados do usuario: </h1>
+      {isLogged ? 'Meu primeiro componente de usuário' : 'Não está logado'}
 
-      <UserInfo />
+      {isLogged && <UserInfo />}
+      {/* {isLogged ? <UserInfo /> : <h1>Não esta logado:</h1>} */}
     </div>
   )
+
+
+
+
 }
 
 export default App;
