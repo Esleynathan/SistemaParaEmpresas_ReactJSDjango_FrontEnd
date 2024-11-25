36eb6e51e064a4d5e0eed7dbf3e63ddfaee61770
Esley <esleynathan@hotmail.com>
2024-11-25 14:36:40 -0300

    Capturando seu primeiro evento

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  // function clicou() {
+  //   alert('Clicou aqui!')
+  // }
+
+  return (
+    <div>
+      <button
+        onClick={() => alert('Clicou aqui!')}
+      >
+        Clique aqui
+      </button>
+    </div >
+  )
+}
+
+export default App
