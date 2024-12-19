84c31fcd381a140f76eb1006c9af49e1b0db4403
Esley <esleynathan@hotmail.com>
2024-12-19 16:16:36 -0300

    Arrays no State: Adicionando um elemento

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const [fullName, setFullName] = useState({ firstName: '', lastName: '' })
+  const [tasks, setTasks] = useState<string[]>([])
+  const [inputValue, setInputValue] = useState('')
 
-  const handleChangeFirstName = (e: ChangeEvent<HTMLInputElement>) => {
-    setFullName({ ...fullName, firstName: e.target.value })
+  const handleadd = () => {
+    if (inputValue == '') return;
+
+    // Método 1:
+    // setTasks ([...tasks, inputValue])
+
+    // Método 2:
+    // setTasks(tasks.concat([inputValue]))
+
+    // Método 3:
+    const newTasks = [...tasks]
+    newTasks.push(inputValue)
+    setTasks(newTasks)
+
+    setInputValue('')
   }
 
-  const handleChangeLastName = (e: ChangeEvent<HTMLInputElement>) => {
-    setFullName({ ...fullName, lastName: e.target.value })
-  }
 
   return (
       <div>
-        <input
-          value={fullName.firstName}
-          onChange={handleChangeFirstName}
-        />
+        <h1>Minha lista de tarefas:</h1>
+        <div>
+          <input
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+          />
 
-        <input
-          value={fullName.lastName}
-          onChange={handleChangeLastName}
-        />
-      </div >
+          <button onClick={handleadd}>
+            Adicionar
+          </button>  
+        </div>
+
+
+        <div>
+          <ul>
+            {tasks.map((value, key) => (
+              <li key={key}>
+                {value}
+
+              </li>
+              
+            ))}
+          </ul>
+        </div>
+    </div>
   )
 }
 
