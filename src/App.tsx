c4abf766cd5ef1074d0c86ce0cb4fb2fb26647a4
Esley <esleynathan@hotmail.com>
2025-01-24 17:34:53 -0300

    Axios: Manipulando erros

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,24 @@
-import axios from "axios"
-
-// type Post = {
-//   userId: number;
-//   id: number;
-//   title: string;
-//   body: string;
-// }
+import axios, { Axios, AxiosError } from "axios"
 
 const App = () => {
-  // const handleGetPosts = async () => {
-  //   const posts = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-  //     params: {
-  //       parametro1: 'valor1',
-  //       parametro2: 'valor2',
-  //     }
-  // })
 
-
-  // const handleGetPosts = async () => {
-  //   const posts = await axios.post('https://jsonplaceholder.typicode.com/posts', {
-  //     title: 'Meu novo post',
-  //     body: 'Conteudo do meu post',
-  //     userId: 1
-  // })
-
-
-  // const handleGetPosts = () => {
-  //   axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
-  //     .then(response => {
-  //       console.log(response.data[0].title)
-  //     })
-  // }
-  
   const api = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/'
   })
 
-  // axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/'
 
   const handleGetPosts = async () => {
-    const posts = await api.get('posts')
-    console.log(posts.data)
+    // const posts = await api.get('posts').catch(response => { console.log(response)})
 
+    try{
+      const posts = await api.get('posts')
+      console.log(posts.data)
+      
+    } catch (e){
+
+      const error = e as AxiosError
+      console.log('O erro foi:', error.message)
+    }
   }
 
 
