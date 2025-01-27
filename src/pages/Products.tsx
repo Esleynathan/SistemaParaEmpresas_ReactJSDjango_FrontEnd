cfe9f8bd8870e888d02dd91ebc621e94ec20ad14
Esley <esleynathan@hotmail.com>
2025-01-27 12:31:01 -0300

    Lidando com parâmetros de consulta

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,15 +1,32 @@
-import { useNavigate } from "react-router-dom"
+import { useSearchParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+
 
 export const Products = () => {
-    const navigate = useNavigate()
-    const handleOnClick = () => {
-        navigate('/products/categoria/1')
-    }
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const [count, setCount] = useState(0)
+
+    const handleOnClick = () => setCount(count + 1)
+
+    useEffect(() => {
+        // Método 1: Ele exclui todos os querys strings
+        // setSearchParams({ count: String(count)})
+
+        // Método 2: Preserva todas as querystrings
+        // searchParams.set('count', String(count))
+        // setSearchParams(searchParams)
+
+        console.log(searchParams.get('parametro2'))
+        setSearchParams(searchParams)
+
+    }, [count])
+
     return (
         <div>
             <h1>Essa é a página PRODUTOS</h1>
 
-            <button onClick={handleOnClick}>Ir para um produto</button>
+            <button onClick={handleOnClick}>Adicionar mais um</button>
         </div>
     )
 }
