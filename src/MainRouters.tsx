29f725e6ddf6f443c4666b4f9033df169fb60a7c
Esley <esleynathan@hotmail.com>
2025-01-28 10:56:35 -0300

    Outra forma de criar rotas

--- src/MainRouters.tsx
+++ src/MainRouters.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from "react-router-dom"
+import { Routes, Route, Outlet, useRoutes } from "react-router-dom"
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Product } from "./pages/Product";
@@ -7,28 +7,70 @@ import { NotFoundPage } from "./pages/NotFoundPage";
 import { AuthMiddleware } from "./middlewares/AuthMiddleware";
 import { Login } from "./pages/Login";
 
+
+// Método 1
+// export const MainRouters = () => {
+//     return ( 
+//         <Routes>
+//         <Route path='*' element={ <NotFoundPage /> } />
+//         <Route index element={ <Home /> } />
+//         <Route path='/about' element={ <About /> } />
+
+
+//         <Route 
+//             path='/products' 
+//             element={
+//                 <AuthMiddleware>
+//                 <Outlet />
+//                 </AuthMiddleware>
+//             }>
+
+//             <Route element={ <Products /> } />
+//             <Route path=':categoryId/:productId' element={ <Product /> } />
+//         </Route>
+
+
+//         <Route path='/login' element={ <Login /> } />
+//     </Routes>
+//     )
+// }
+
+
+// Método 2 (Objetos JAVASCRIPT)
 export const MainRouters = () => {
-    return ( 
-        <Routes>
-        <Route path='*' element={ <NotFoundPage /> } />
-        <Route index element={ <Home /> } />
-        <Route path='/about' element={ <About /> } />
+    const element = useRoutes([
+        {
+            index: true,
+            element: <Home />
+        },
+        {
+            path: '/about',
+            element: <About />
+        },
+        {
+            path: '/products',
+            element: <AuthMiddleware> <Outlet /> </AuthMiddleware>,
+            children: [
+                {
+                    index: true,
+                    element: <Products />
+                },
+                {
+                    path: ':categoryId/:productId',
+                    element: <Product />
+                },
+            ]
+        },
+        {
+            path: '/login',
+            element: <Login />
+        },
+        {
+            path: '*',
+            element: <NotFoundPage />
+        }
+        
+    ])
 
-
-        <Route 
-            path='/products' 
-            element={
-                <AuthMiddleware>
-                <Outlet />
-                </AuthMiddleware>
-            }>
-
-            <Route element={ <Products /> } />
-            <Route path=':categoryId/:productId' element={ <Product /> } />
-        </Route>
-
-
-        <Route path='/login' element={ <Login /> } />
-    </Routes>
-    )
+    return element;
 }
