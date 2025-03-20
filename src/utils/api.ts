90e02fa0b7152e16f123b5bb1b961c438b29e780
Esley <esleynathan@hotmail.com>
2025-03-20 13:03:13 -0300

    Deploy - Preparando o projeto ReactUS para o deploy na DigitalOcean

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,7 @@ import { ApiError } from "src/models/Api";
 import { handleGetAccessToken } from "./auth";
 
 const BASE_URL = 'http://127.0.0.1:8000/api/v1';
+// const BASE_URL = 'process.env.REACT_APP_API_BASE_URL';
 
 export const useApi = async <TypeDataResponse> (
     endpoint: string,
